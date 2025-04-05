cfbbdc942f990d906e138ad096db4b6e2a7ea7fb
Moshtafizur-Rahman-Anty <alphaaa.90@gmail.com>
2025-04-05 08:35:49 +0600

    Merge branch 'master' of https://github.com/Moshtafizur-Rahman-Anty/Soical-site-using-PHP-and-MYSQL
    Refactor: Organize register handler and database files into separate subfolders

--- index.php
+++ index.php
@@ -1,14 +1,7 @@
  
  <?php
+require 'config/config.php';
 
-$con = mysqli_connect("localhost", "root", "", "social");
-
-if(mysqli_connect_errno())  {
-   echo "Failed to connect " . mysqli_connect_errno();
-}
-
-
-$query = mysqli_query($con, "INSERT INTO test (name) VALUES ('Test1')");
 
 ?>
  
